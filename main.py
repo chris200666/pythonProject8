0ace171d2de97fed233519e042e41756bd5d997b
chris2000666 <wangtianle0808@qq.com>
2023-11-13 22:36:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+x=int(input("give me a number SMALLER than 10"))
+
+
+if x<10:
+    print("THANK YOU")
+#example
+#
+x=int(input("give me a number SMALLER than or equal to 10 but greater than ora equal to5 "))
+
+if x>10:
+    print("no you picked a number too big")
+elif x<5:
+    print("no,you picked a number is too small")
+else:
+    print("THANK YOU")
+
+#example判斷一個溫度是否合適。只會出現一個結果
+temp=int(input("give me the temperature" ))
+if temp>25:
+   print("lts too hot")
+elif temp<20:
+    print("lts too cold")
+else:
+    print("the temperature is perfect")
+
+x = int(input("give me an integer"))
+#可能產生的多個結果。但是這種結果是不會讓出現的
+if x>1000:
+    print("wow, you x is really large ,are you sure ok")
+if x>100:
+    print("That is a big x,okay")
+if x>10:
+    print("good x")
+if x<=10:
+    print("perfect x")
+
+    x=int(input("give me an integer"))
+#判斷各個結果不同的區域
+if x>1000:
+        print("wow your x is really large.are you sure ok")
+elif x>100:
+        print("That is a big x okay")
+elif x>10:
+        print("good x")
+else:
+        print("perfect x")
+#homework
+#write a programmer #Write a program that asks for the a, b and c of a QuadraƟc EquaƟon it should say
+#how many roots there are and give the roots. But only if the roots are real of course.
+#You DO NOT need to use DEF but it would be really cool if you did
+
+
+
+
+
+
+
+
+
+
+
+
